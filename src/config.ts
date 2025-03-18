3e5564415706094323e27cec51f35298145abf7b
Administrator <admin@example.com>
2025-03-18 18:27:31 +0800

    remove

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export const config = {
     supportLink: "https://t.me/danil_kho",
     pinataJwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4YWQ5ODgzYi1kMDk1LTRlMjItODkwZS00YWNiZDhmZWFjNTkiLCJlbWFpbCI6InRoZW1hdGVyYm9veEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYTk3Y2RlNGQ2ODI0ZmE4MWFkNmIiLCJzY29wZWRLZXlTZWNyZXQiOiI5NDYwY2E0ZmViOTFkNDVlODIxZWY3ZWMwMzYxMTJlZTA3NDEwZTFhMjk2YThmZWQ2MGI2Yzc1NTE0MWJmNTY2IiwiZXhwIjoxNzczNzE2MjY3fQ.1-CXGNcFJ9A0_cLrsVCDhZcbHAgwlQ-gdmK-AOwfqJk",
     pinataGateway: "lime-adequate-gopher-535.mypinata.cloud",
-    comissionAmount: 200_000_000,
+    //comissionAmount: 200_000_000,
+    comissionAmount: 2000,
     feeReceiver: "2aV6AVXvE5SxURyNUDTc8SH2Ryo9CwHW1Z7TdokLYBZe",
 };
