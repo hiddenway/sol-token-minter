bab474b566c071e599d7ddfc30d4439810a95805
Administrator <admin@example.com>
2025-03-18 18:18:13 +0800

    update address

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
-})
+  build: {
+    minify: "terser", // Используем Terser
+    terserOptions: {
+      compress: {
+        drop_console: true, // Удаляет console.log
+      },
+      mangle: true, // Переименовывает переменные
+      format: {
+        comments: false, // Убирает комментарии
+      },
+    },
+  },
+});
