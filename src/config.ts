cdc73c3ab592066e97360fc8a4f13a702349fefe
Administrator <admin@example.com>
2025-03-19 17:20:46 +0800

    ad

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    network: "https://convincing-black-panorama.solana-mainnet.quiknode.pro/2a1ebe2edf6a7c90d3b27f20acf58561b3aefca9/",
-    // network: "https://api.devnet.solana.com",
+    //network: "https://convincing-black-panorama.solana-mainnet.quiknode.pro/2a1ebe2edf6a7c90d3b27f20acf58561b3aefca9/",
+    network: "https://api.devnet.solana.com",
     ipfsGateway: "https://dweb.link/ipfs",
     supportLink: "https://t.me/danil_kho",
     pinataJwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4YWQ5ODgzYi1kMDk1LTRlMjItODkwZS00YWNiZDhmZWFjNTkiLCJlbWFpbCI6InRoZW1hdGVyYm9veEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYTk3Y2RlNGQ2ODI0ZmE4MWFkNmIiLCJzY29wZWRLZXlTZWNyZXQiOiI5NDYwY2E0ZmViOTFkNDVlODIxZWY3ZWMwMzYxMTJlZTA3NDEwZTFhMjk2YThmZWQ2MGI2Yzc1NTE0MWJmNTY2IiwiZXhwIjoxNzczNzE2MjY3fQ.1-CXGNcFJ9A0_cLrsVCDhZcbHAgwlQ-gdmK-AOwfqJk",
